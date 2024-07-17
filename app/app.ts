6217f005cf9d376b799254169f4eb33f2afc9b9b
Nickollas <nickollassilva90@gmail.com>
2024-07-17 16:45:26 -0300

    adicionando typescript no projeto

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,6 @@
 import { Negociacao } from "./models/negociacao.js";
 alert("welcome to the mato");
 
-const negociacao = new Negociacao(new Date(), 10, 1000);
+const negociacao = new Negociacao(new Date());
+negociacao.quantidade = 2;
 console.log(negociacao.quantidade);
