1bef966a9c39873353764627842dc4e9a93fa2bc
Nickollas <nickollassilva90@gmail.com>
2024-08-02 16:14:25 -0300

    criando enums e definindo seus valores

---
+++ app/enums/dias-da-semana.ts
@@ -0,0 +1,11 @@
+export enum DiasDaSemana {
+  DOMINGO = 0,
+  SEGUNDA = 1,
+  TERCA = 2,
+  QUARTA = 3,
+  QUINTA = 4,
+  SEXTA = 5,
+  SABADO = 6,
+}
+
+//o typescript define valores para cada elemento da enum por padrão, seguindo uma sequencia, mas é melhor que nós mesmos definamos isso
