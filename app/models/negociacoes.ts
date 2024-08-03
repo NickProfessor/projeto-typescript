a081dbeee4cb42230a0059cf344292f9b587c4a5
Nickollas <nickollassilva90@gmail.com>
2024-08-03 11:03:42 -0300

    deixando a visibilidade do metodo explícito

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -3,12 +3,12 @@ import { Negociacao } from "./negociacao.js";
 export class Negociacoes {
   private negociacoes: Negociacao[] = []; //lista que vai armazenar as negociações
 
-  adiciona(negociacao: Negociacao) {
+  public adiciona(negociacao: Negociacao) {
     // método capaz de adicionar uma negociao a lista de negociacoes
     this.negociacoes.push(negociacao); // menciona a lista de negociacoes da instancia, e através do método push adiciona mais uma negociacao
   }
 
-  lista(): ReadonlyArray<Negociacao> {
+  public lista(): ReadonlyArray<Negociacao> {
     // método capaz de trazer a lista de negociações apenas para leitura, sem possibilidade de alteração
     return this.negociacoes; // retorna a lista de negociacoes
   }
